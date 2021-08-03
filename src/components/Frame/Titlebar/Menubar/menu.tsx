1ebf69eaab5c0bda1b136848fa27add1ddda443f
Jane <727751354@qq.com>
2021-08-03 14:10:29 +0800

    menus 完善

--- src/components/Frame/Titlebar/Menubar/menu.tsx
+++ src/components/Frame/Titlebar/Menubar/menu.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenubarItem } from '@/types/index.d'
+import menuConctrol from '@/components/Frame/Titlebar/Menubar/MenuConctrol'
 
 export default function Menu({ menu }: { menu: () => MenubarItem }) {
 
-  const item = menu();
-  console.log(item);
+  const [childrenView, setChildrenView] = useState(false);
 
-  return <div className="menubar-menu-button">
-    <div className="menubar-menu-title">{item.title}</div>
+  menuConctrol.add(setChildrenView);
+
+  const item = menu();
+
+  const Children = (
+    <div className="menubar-menu-children">
+      {item.children.map((v, i) =>
+        v.type === 'separator' ? <span className="menubar-menu-children-separator block" key={i}></span> : <p className="menubar-menu-children-title mar nowrap" key={i} onClick={v.onClick}>{v.title}</p>
+      )}
+    </div>
+  )
+
+  function changeChildrenView() {
+    setChildrenView(!childrenView)
+  }
+
+  return <div className="menubar-menu-button relative flex-center">
+    <div className={childrenView ? 'menubar-menu-title menubar-menu-active' : 'menubar-menu-title'} onClick={changeChildrenView}>{item.title}</div>
+    {childrenView ? Children : ""}
   </div>
 }
