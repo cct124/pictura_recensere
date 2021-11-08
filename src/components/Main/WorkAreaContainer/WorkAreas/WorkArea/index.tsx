edc54a165f3be3059cc20e1b6e44bbec06f06fd6
huangdonghao <huangdonghao@miya818.com>
2021-11-08 15:19:19 +0800

    删除了原有的canvas实现方案采用Konva库重构

--- src/components/Main/WorkAreaContainer/WorkAreas/WorkArea/index.tsx
+++ src/components/Main/WorkAreaContainer/WorkAreas/WorkArea/index.tsx
@@ -1,39 +1,17 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Style from "./index.modules.scss";
 import { classNames } from '@/utils/tool';
-import CanvasGroup from "./CanvasGroup";
-import WorkAreaInfoBar from "./WorkAreaInfoBar";
-import Layer from "./Layer";
-import { WorkAreaType } from "@/types/type.d";
-import MatrixConctrol from "@/plugin/canvas/matrixConctrol";
-import CanvasConctrol, { CanvasConctrolEvent, CanvasConctrolEventName } from "@/plugin/canvas/CanvasConctrol";
-import LayerConctrol, { layerEvent, LayerType } from "@/plugin/canvas/layerConctrol";
-import ToolsConctrol, { Tool, ToolsConctrolEventName } from "@/plugin/canvas/toolsConctrol";
-import InteractiveConctrol, { InteractiveEventName, RectParamsType } from "@/plugin/canvas/interactiveConctrol";
 
 /**
  * 工作区
  * @returns 
  */
-export default function WorkArea({ canvasInfo, toolsConctrol }: { toolsConctrol: ToolsConctrol, canvasInfo: WorkAreaType }) {
+export default function WorkArea() {
 
-  const style = canvasInfo.active ? {} : { display: 'none' };
-
-  const [zoomPercent, setZoomPercent] = useState('100%');
-
-  const [layerConctrol, setLayerConctrol] = useState<LayerConctrol>(null);
-
-  const [interactiveConctrol, setInteractiveConctrol] = useState<InteractiveConctrol>(null);
 
   return (
-    <div className={classNames(Style.workArea, 'flex')} style={style}>
-      <div className={classNames(Style.leftPartition)}>
-        <CanvasGroup />
-        <WorkAreaInfoBar zoomPercent={zoomPercent} />
-      </div>
-      <div className={classNames(Style.rightPartition)}>
-        <Layer canvasConctrol={layerConctrol} />
-      </div>
+    <div className={classNames(Style.workArea, 'flex')}>
+
     </div>
   )
 }
--- src/components/Main/WorkAreaContainer/WorkAreas/index.tsx
+++ src/components/Main/WorkAreaContainer/WorkAreas/index.tsx
@@ -51,9 +51,9 @@ export default function WorkAreas({ workAreas, setWorkAreas, tabsStack, setTabsS
       <Tools toolsConctrol={toolsConctrol} />
       <div className={classNames(Style.WorkAreaTabs)}>
         <WorkAreaTabs workAreas={workAreas} setWorkAreas={setWorkAreas} tabsStack={tabsStack} setTabsStack={setTabsStack} />
-        {/* {
-          workAreas.map((canvasInfo) => <WorkArea toolsConctrol={toolsConctrol} canvasInfo={canvasInfo} key={canvasInfo.id} />)
-        } */}
+        {
+          workAreas.map((canvasInfo) => <WorkArea key={canvasInfo.id} />)
+        }
       </div>
     </div>
   )
